ed62dd8d10abcda913bdf225476cac72f6367bdb
Paw <76894433+stolenfallen1@users.noreply.github.com>
2024-01-27 15:35:04 +0800

    feature: display icons and card overlay color base on the title

---
+++ src/utils/bg-color-mapping.ts
@@ -0,0 +1,10 @@
+const bgColorMapping: Record<string, string> = {
+    Work: "bg-lightred",
+    Play: "bg-softblue",
+    Study: "bg-pinkred",
+    Exercise: "bg-limegreen",
+    Social: "bg-darkviolet",
+    "Self Care": "bg-softorange",
+};
+
+export default bgColorMapping;
