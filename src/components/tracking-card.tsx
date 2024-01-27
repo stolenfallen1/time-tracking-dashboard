a6c8fd78138727aac7f128966c5271f4da3a0a12
Paw <76894433+stolenfallen1@users.noreply.github.com>
2024-01-27 17:46:13 +0800

    refactor: layout responsiveness done

--- src/components/tracking-card.tsx
+++ src/components/tracking-card.tsx
@@ -27,11 +27,11 @@ export default function TrackingCard({
     timeFrameLabel,
 }: TrackingCardProps) {
     return (
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-2 lg:gap-4">
             {trackingData.map((data, index) => (
                 <div
                     key={index}
-                    className="relative overflow-hidden w-60 h-48 rounded-lg p-4 flex flex-col gap-10"
+                    className="relative overflow-hidden w-[295px] h-40 sm:w-[204px] sm:h-48 lg:w-60 lg:h-52 rounded-lg pt-10 flex flex-col"
                 >
                     <div
                         className={`absolute top-0 left-0 right-0 bottom-0 rounded-lg ${
@@ -45,18 +45,20 @@ export default function TrackingCard({
                         />
                     </div>
                     <div
-                        className="relative z-10 flex flex-col gap-4 bg-darkblue"
+                        className="relative h-52 z-10 flex flex-col gap-4 bg-darkblue rounded-t-lg p-4"
                         style={{ position: "relative", zIndex: 2 }}
                     >
                         <div className="flex justify-between">
-                            <h1>{data.title}</h1>
-                            <p>...</p>
+                            <h1 className="text-white mt-4 sm:mt-0">
+                                {data.title}
+                            </h1>
+                            <p className="text-white">...</p>
                         </div>
-                        <div className="flex flex-col gap-4">
-                            <p className="text-5xl">
+                        <div className="flex flex-row gap-20 items-center sm:flex-col sm:gap-4 sm:items-start">
+                            <p className="text-xl sm:text-3xl lg:text-5xl text-white">
                                 {data.timeframes[selectedTimeFrame].current} hrs
                             </p>
-                            <p>{`${timeFrameLabel} - ${data.timeframes[selectedTimeFrame].previous}`}</p>
+                            <p className="text-white/50">{`${timeFrameLabel} - ${data.timeframes[selectedTimeFrame].previous}`}</p>
                         </div>
                     </div>
                 </div>
